9d356d12b73b03ff3c4be1a3be0467d5a19ef944
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-05-13 16:44:56 -0400

    Initial Commit

---
+++ Quests/QuestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestsApp.swift
+//  Quests
+//
+//  Created by Travis Luckenbaugh on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
